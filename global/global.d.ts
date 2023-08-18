93f43c9188b8a9f6a4ba5a7c64bcfe98228f3f75
FrostIce482 <frostice48260@gmail.com>
2023-08-18 16:16:15 +0700

    move pack global types to global

--- global/global.d.ts
+++ global/global.d.ts
@@ -1,2 +1,7 @@
 type LogLevel = 'log' | 'warn' | 'info' | 'error'
 type LogLevelOrUnknown = 'log' | 'warn' | 'info' | 'error' | 'unknown'
+
+type Replace<A extends object, B extends object> = Omit<A, keyof B> & B
+type ReplaceValue<A extends object, K extends keyof A, B> = Omit<A, K> & Record<K, B>
+type RequiredSome<O, K extends keyof O> = O & { [X in K]-?: O[X] }
+type ArrayOrReadonly<T> = T[] | readonly T[] | []
--- pack/types/global.d.ts
+++ pack/types/global.d.ts
@@ -11,8 +11,3 @@ interface Function {
     get fileName(): string
     get lineNumber(): number
 }
-
-type Replace<A extends object, B extends object> = Omit<A, keyof B> & B
-type ReplaceValue<A extends object, K extends keyof A, B> = Omit<A, K> & Record<K, B>
-type RequiredSome<O, K extends keyof O> = O & { [X in K]-?: O[X] }
-type ArrayOrReadonly<T> = T[] | readonly T[] | []
