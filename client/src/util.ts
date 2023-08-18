8267d5048c750044c91bb40ba27e7ac34133afc4
FrostIce482 <frostice48260@gmail.com>
2023-08-18 19:29:03 +0700

    add evalthrow & fix eval fail

--- client/src/util.ts
+++ client/src/util.ts
@@ -1,10 +1,16 @@
 import { fetchThrow } from "./lib/misc.js";
 
 export async function sendEval(script: string) {
-    const res = await fetchThrow('/session/eval', {
+    const res = await fetchThrow('/sendeval', {
         method: 'POST',
         body: script
     })
 
     return await res.json() as Bedrock.Events['eval']
+}
+
+export async function sendEvalThrowable(script: string) {
+    const res = await sendEval(script)
+    if (res.error) throw new Error('Eval failed', { cause: res })
+    return res
 }
