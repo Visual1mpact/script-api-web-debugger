a81ac430d15dc5b3a00b209710a78112a98a724b
FrostIce482 <frostice48260@gmail.com>
2023-09-02 07:43:44 +0700

    allow `server-net` and `server-admin` on hook

--- pack/tool/hook.js
+++ pack/tool/hook.js
@@ -9,23 +9,58 @@ dir = path.isAbsolute(dir) ? dir : path.join(process.cwd(), '..', dir)
 
 console.time('hooked')
 
-console.log('getting pack uuid & version...')
+/* ----- ----- ----- ----- */ console.log('getting pack uuid & version...')
+
+// reads pack manifest & parse
 const packdata = JSON.parse(fs.readFileSync('pack/manifest.json').toString())
+// gets pack uuid & version
 const { uuid: packuuid, version } = packdata.header
 
-console.log('reading server.properties...')
+/* ----- ----- ----- ----- */ console.log('reading server.properties...')
+
+// reads server properties
 const serverProperties = fs.readFileSync(path.join(dir, 'server.properties')).toString()
+// gets level-name config
 const levelName = serverProperties.match(/^level-name=(.*)/m)?.[1]
 
-console.log(`listing pack to world's behavior packs...`)
+/* ----- ----- ----- ----- */ console.log('listing pack to world\'s behavior packs...')
+
+// directory for bds world's behavior pack list (world_behavior_packs.json)
 const packsDir = path.join(dir, 'worlds', levelName, 'world_behavior_packs.json')
+
+// read bds world's behavior pack list
 const packs = JSON.parse(fs.readFileSync(packsDir).toString())
+// add pack to bds world's behavior pack list
 const packsNew = packs.filter(v => v.pack_id !== packuuid).concat([{ pack_id: packuuid, version }])
+// write bds world's behavior pack list
 fs.writeFileSync(packsDir, JSON.stringify(packsNew))
 
-console.log(`adding pack to development behavior packs...`)
+/* ----- ----- ----- ----- */ console.log('adding pack to development behavior packs...')
+
+// directory for bds development behavior packs (development_behavior_packs)
 const devDir = path.join(dir, 'development_behavior_packs', packuuid)
+
+// remove if exist
 fs.rmSync(devDir, { force: true, recursive: true })
+// copy pack to bds development behavior packs
 fs.cpSync('pack', devDir, { recursive: true, force: true })
+
+/* ----- ----- ----- ----- */ console.log('allowing modules...')
+
+// diretory for bds pack configs
+const confDir = path.join(dir, 'config', packuuid)
+
+// make dir
+fs.mkdirSync(confDir, { force: true, recursive: true })
+// write
+fs.writeFileSync(path.join(confDir, 'permissions.json'), JSON.stringify({
+    allowed_modules: [
+        "@minecraft/server",
+        "@minecraft/server-gametest",
+        "@minecraft/server-ui",
+        "@minecraft/server-net",
+        "@minecraft/server-admin",
+    ]
+}))
 
 console.timeEnd('hooked')
--- pack/tool/unhook.js
+++ pack/tool/unhook.js
@@ -9,22 +9,44 @@ dir = path.isAbsolute(dir) ? dir : path.join(process.cwd(), '..', dir)
 
 console.time('unhooked')
 
-console.log('getting pack uuid...')
+/* ----- ----- ----- ----- */ console.log('getting pack uuid...')
+
+// reads pack manifest & parse
 const packdata = JSON.parse(fs.readFileSync('pack/manifest.json').toString())
+// gets pack uuid & version
 const { uuid: packuuid } = packdata.header
 
-console.log('reading server.properties...')
+/* ----- ----- ----- ----- */ console.log('reading server.properties...')
+
+// reads server properties
 const serverProperties = fs.readFileSync(path.join(dir, 'server.properties')).toString()
+// gets level-name config
 const levelName = serverProperties.match(/^level-name=(.*)/m)?.[1]
 
-console.log(`unlisting pack to world's behavior packs...`)
+/* ----- ----- ----- ----- */ console.log('unlisting pack to world\'s behavior packs...')
+
+// directory for bds world's behavior pack list (world_behavior_packs.json)
 const packsDir = path.join(dir, 'worlds', levelName, 'world_behavior_packs.json')
+
+// read bds world's behavior pack list
 const packs = JSON.parse(fs.readFileSync(packsDir).toString())
+// remove pack to bds world's behavior pack list
 const packsNew = packs.filter(v => v.pack_id !== packuuid)
+// write bds world's behavior pack list
 fs.writeFileSync(packsDir, JSON.stringify(packsNew))
 
-console.log(`deleting pack to development behavior packs...`)
+/* ----- ----- ----- ----- */ console.log('deleting pack to development behavior packs...')
+
+// directory for bds development behavior packs (development_behavior_packs)
 const devDir = path.join(dir, 'development_behavior_packs', packuuid)
+// remove
 fs.rmSync(devDir, { force: true, recursive: true })
+
+/* ----- ----- ----- ----- */ console.log('disallowing modules...')
+
+// diretory for bds pack configs
+const confDir = path.join(dir, 'config', packuuid)
+// remove
+fs.rmSync(confDir, { force: true, recursive: true })
 
 console.timeEnd('unhooked')
