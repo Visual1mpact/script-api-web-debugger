b817efed301a940799216aad3dd32a5022b17776
FrostIce482 <frostice48260@gmail.com>
2023-09-13 21:25:21 +0700

    more

--- client/src/tabs/eval.ts
+++ client/src/tabs/eval.ts
@@ -73,6 +73,123 @@ export async function sendInput(value: string) {
     }
 }
 
+// inputs
+
+input.addEventListener('keydown', ev => {
+    const { charCode, keyCode, which, ctrlKey, shiftKey } = ev
+
+    switch (charCode || keyCode || which) {
+        case 13: {
+            if (!input.value || !ctrlKey) return
+
+            ev.preventDefault()
+            sendInput(input.value)
+        } break
+
+        case 9: {
+            if (ctrlKey) return
+
+            ev.preventDefault()
+            const { selectionStart: start, selectionEnd: end, value } = input
+
+            // get selection, lines, initial line length
+            const lines = value.split('\n')
+            const { startIndex, endIndex, selectedLines } = selectLines(lines, start, end)
+            const initLen = selectedLines.reduce((a, b) => a + b.length, 0), initFirstLen = selectedLines.at(-1)?.length ?? 0
+
+            // map lines
+            const mapped = selectedLines.map(v => shiftKey ? v.replace(/^ {0,4}/, '') : '    ' + v)
+            lines.splice(startIndex, endIndex - startIndex, ...mapped)
+
+            // calculate delta length
+            const deltaLen = mapped.reduce((a, b) => a + b.length, 0) - initLen,
+                deltaFirstLen = (mapped.at(-1)?.length ?? 0) - initFirstLen
+
+            // set value & selection
+            input.value = lines.join('\n')
+            input.setSelectionRange(start + deltaFirstLen, end + deltaLen, input.selectionDirection)
+        } break
+    }
+})
+
+input.addEventListener('beforeinput', (ev) => {
+    const { inputType } = ev, char = ev.data ?? ''
+    const { selectionStart: start, selectionEnd: end, value } = input
+
+    switch (inputType) { 
+        case 'insertLineBreak': {
+            // line, current line, current tab level, suffix level
+            const lines = value.split('\n')
+            const currentLine = selectLines(lines, start)
+            let tabLevel = currentLine.selectedLines[0]?.match(/^ */)?.[0].length ?? 0
+            let suffixTabLevel: number | undefined
+
+            // value trimmed value, boundary
+            const [valueStart, valueEnd] = sliceOutside(value, start, end)
+            const bndStart = boundaryType(valueStart.replace(/ *$/, '').at(-1)),
+                bndEnd = boundaryType(valueEnd.replace(/^ */, '').at(0))
+
+            switch (bndStart) {
+                case 'open': {
+                    if (bndEnd === 'close') suffixTabLevel = tabLevel
+                    tabLevel += 4
+                } break
+
+                case 'close': {
+                    if (bndEnd === 'open') suffixTabLevel = tabLevel
+                    else tabLevel -= Math.max(tabLevel - 4, 0)
+                } break
+            }
+
+            // set value
+            const nvStart = valueStart + ('\n' + ' '.repeat(tabLevel)),
+                nvEnd = (suffixTabLevel !== undefined ? '\n' + ' '.repeat(suffixTabLevel) : '') + valueEnd
+            
+            input.value = nvStart + nvEnd
+            input.setSelectionRange(nvStart.length, nvStart.length, 'none')
+
+            ev.preventDefault()
+            return
+        } return
+        
+        case 'insertText': {
+            // char is closing bracket, start selection === end selection
+            if (char in bracketCloses && start === end && value[end] === char) {
+                input.setSelectionRange(start + 1, start + 1, 'none')
+
+                ev.preventDefault()
+                return
+            }
+
+            // char is bracket pair
+            const bracket = bracketPairs[char]
+            if (bracket) {
+                const [nvStart, nvEnd] = sliceOutside(value, start, end)
+                const selection = value.slice(start, end)
+
+                input.value = nvStart + char + selection + bracket + nvEnd
+                input.setSelectionRange(start + 1, end + 1, input.selectionDirection)
+
+                ev.preventDefault()
+                return
+            }
+        } return
+
+        case 'deleteContentBackward': {
+            if (start === end) {
+                const [bndStart, bndEnd] = value.slice(start - 1, start + 1)
+                if (bndStart && bracketPairs[bndStart] === bndEnd) {
+                    input.value = sliceOutside(value, start - 1, start + 1).join('')
+                    input.setSelectionRange(start - 1, start - 1, )
+
+                    ev.preventDefault()
+                    return
+                }
+            }
+        } return
+    }
+})
+
 function selectLines(lines: string[], start: number, end = start) {
     let ptr = 0, startIndex = 0, endIndex = 0
     if (start > end) [start, end] = [end, start]
@@ -109,79 +226,29 @@ function boundaryType(char = ' ') {
         : false
 }
 
-// inputs
+function sliceOutside(input: string, start: number, end: number): [string, string] {
+    return [input.slice(0, start), input.slice(end)]
+}
 
-input.addEventListener('keydown', ev => {
-    const { charCode, keyCode, which, ctrlKey, shiftKey } = ev
+const bracketPairs: Record<string, string> = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+    '"': '"',
+    "'": "'",
+    '`': '`'
+}
+Object.setPrototypeOf(bracketPairs, null)
 
-    switch (charCode || keyCode || which) {
-        case 13: {
-            ev.preventDefault()
-
-            // control key
-            if (ctrlKey) {
-                const val = input.value
-                if (val) sendInput(val)
-                break
-            }
-
-            // get selection, line, current line
-            const { selectionStart: start, selectionEnd: end, value } = input
-            const lines = value.split('\n')
-            const currentLine = selectLines(lines, start)
-
-            // get current tab level, suffix level
-            let tabLevel = currentLine.selectedLines[0]?.match(/^ */)?.[0].length ?? 0
-            let suffixTabLevel = 0
-
-            // value trimmed value, boundary
-            const valueStart = value.slice(0, start), valueEnd = value.slice(end)
-            const bndStart = boundaryType(valueStart.replace(/ *$/, '').at(-1)), bndEnd = boundaryType(valueEnd.replace(/^ */, '').at(0))
-
-            switch (bndStart) {
-                case 'open': {
-                    if (bndEnd === 'close') suffixTabLevel = tabLevel
-                    tabLevel += 4
-                } break
-
-                case 'close': {
-                    if (bndEnd === 'open') suffixTabLevel = tabLevel
-                    else tabLevel -= 4
-                } break
-            }
-
-            // set value
-            const nvStart = valueStart + '\n' + ' '.repeat(tabLevel), nvEnd = (suffixTabLevel ? '\n' + ' '.repeat(suffixTabLevel) : '') + valueEnd
-            input.value = nvStart + nvEnd
-            input.setSelectionRange(nvStart.length, nvStart.length, 'none')
-        } break
-
-        case 9: {
-            if (ctrlKey) break
-            ev.preventDefault()
-
-            // get selection & lines
-            const { selectionStart: start, selectionEnd: end, selectionDirection, value } = input
-            const lines = value.split('\n')
-
-            // get selected lines & calculate initial length
-            const { startIndex, endIndex, selectedLines } = selectLines(lines, start, end)
-            const initLen = selectedLines.reduce((a, b) => a + b.length, 0), initFirstLen = selectedLines.at(-1)?.length ?? 0
-
-            // map lines
-            const mapped = selectedLines.map(v => shiftKey ? v.replace(/^ {0,4}/, '') : '    ' + v)
-            lines.splice(startIndex, endIndex - startIndex, ...mapped)
-
-            // calculate delta length
-            const deltaLen = mapped.reduce((a, b) => a + b.length, 0) - initLen,
-                deltaFirstLen = (mapped.at(-1)?.length ?? 0) - initFirstLen
-
-            // set value & selection
-            input.value = lines.join('\n')
-            input.setSelectionRange(start + deltaFirstLen, end + deltaLen, selectionDirection)
-        }
-    }
-})
+const bracketCloses: Record<string, null> = {
+    ')': null,
+    '}': null,
+    ']': null,
+    '"': null,
+    "'": null,
+    '`': null
+}
+Object.setPrototypeOf(bracketPairs, null)
 
 send.addEventListener('click', () => {
     if (!input.value) return input.focus()
