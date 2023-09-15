c83c4d8b19569dd2056cce645f3ac8a4b54df709
FrostIce482 <frostice48260@gmail.com>
2023-09-15 16:27:07 +0700

    add interpreter to eval

--- server/src/test.ts
+++ server/src/test.ts
@@ -1,9 +1,12 @@
 import express = require('express')
 import NBedrock from "./bedrock.js";
 import { server } from "./server.js";
+import NInterpreter from './interpreter.js';
 
 Object.assign(globalThis, {
     bedrock: NBedrock,
+    interpreter: NInterpreter,
+    int: NInterpreter,
     server,
     express
 })
